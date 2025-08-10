db331d082fc0d024047b455b9b4b03378ff5f0af
Emilio Gomez <moleculax@gmail.com>
2025-08-10 17:28:33 -0300

    Delete restaurant directory

--- restaurant/src/main/java/com/restaurant/restaurant/data/ReunionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.restaurant.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurant.restaurant.models.Reunion;
-
-public interface ReunionRepository extends JpaRepository<Reunion, Long>{
-
-}
